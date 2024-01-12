6bcaa02d566b4efa1cca9c9a70d636f7946b097e
James Adjinwa <jaymee126@gmail.com>
2024-01-12 16:25:03 -0700

    #3 Simplify the script by providing the Ubuntu package link and using only the requests module

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
-import urllib3 as urllib
-from bs4 import BeautifulSoup as bs
-from fake_headers import Headers 
+import requests
 
-zoomurl = "https://zoom.us/support/download"
-headers = Headers(browser="firefox", os="win", headers=True).generate()
+# URLs for Zoom client downloads
+urls = {
+    # 'Windows': 'https://zoom.us/client/latest/ZoomInstaller.exe',
+    # 'MacOS': 'https://zoom.us/client/latest/Zoom.pkg',
+    'Linux': 'https://zoom.us/client/latest/zoom_amd64.deb'
+}
 
-# for i in range(10):
-       # print(headers.generate())
-
-http = urllib.PoolManager()
-page = http.request('GET', zoomurl, headers=headers)
-soup = bs(page.data, features="html.parser")
-#check = soup.findAll('div')
-check = soup.findAll('p', {"class": "setup-finger"}, recursive=True)
-#print(soup.prettify())
-print(check)
-
-"""
-html tags:
-select id="linuxostype"
-    option value="ubu"
-"""
+# Download executable files for each operating system
+for os, url in urls.items():
+    response = requests.get(url)
+    if response.status_code == 200:
+        filename = f'ZoomInstaller_{os.replace(" ", "")}.{url.split(".")[-1]}'
+        with open(filename, 'wb') as file:
+            file.write(response.content)
+        print(f'Download successful for {os}. File {filename} has been saved.')
+    else:
+        print(f"Failed to download {os}. Status code: {response.status_code}")
